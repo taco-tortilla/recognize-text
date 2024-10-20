98826e3437c40a81e7051c3cd3906b2def3e1006
taco-tortilla <tortillataco0@gmail.com>
2024-10-20 22:18:41 +0900

    add: loading comoponent

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+export function Loading() {
+  return (
+    <div className='flex items-center justify-center space-x-4'>
+      <div className='size-2 animate-ping rounded-full bg-indigo-700'></div>
+      <div className='size-2 animate-ping rounded-full bg-indigo-700'></div>
+      <div className='size-2 animate-ping rounded-full bg-indigo-700'></div>
+    </div>
+  );
+}
