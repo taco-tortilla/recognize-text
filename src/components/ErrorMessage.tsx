b727b1c695e77dc0147a706780e7874ce703f23d
taco-tortilla <tortillataco0@gmail.com>
2024-10-20 22:18:18 +0900

    add: error component

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,8 @@
+export function ErrorMessage() {
+  return (
+    <h1 className='text-center text-base font-semibold text-gray-500'>
+      An error has occureed. 💦
+      <br /> Please try again!!
+    </h1>
+  );
+}
