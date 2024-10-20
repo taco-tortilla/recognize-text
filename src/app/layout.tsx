9a8bce0cd84b41e71ee872035b7588fe03202dbf
taco-tortilla <tortillataco0@gmail.com>
2024-10-20 14:24:57 +0900

    chore: add prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "web ocr",
-  description: "OCR web app",
+  title: 'web ocr',
+  description: 'OCR web app',
 };
 
 export default function RootLayout({
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>{children}</body>
     </html>
   );
